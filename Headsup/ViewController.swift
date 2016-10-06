c37521d9c80ec9bc0c318153cc361a680ebffe24
Sgmedical <sgmedical@sgs-macbook-air.local>
2016-10-06 14:50:39 +0800

    1. loop through the questions . 2. close the project

--- Headsup/ViewController.swift
+++ Headsup/ViewController.swift
@@ -10,16 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    /*
+     1. store an array of questions
+     2.
+    */
+    
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var CountLbl: UILabel!
+    
+    var currentQuestionIndex: Int = 0
+    
+    var quesArr = [
+        "What’s the name of this city in the United States with a bell in the near middle of the city?",
+        "What is the name of the spaceship first landed on the moon?",
+        "What does 'umbros' mean in Latin? Translate it into Sentinelese.",
+        "What’s the first photo printed from a camera in the world?",
+        "When was the first email ever sent in the world?",
+        "… and what was that email about?"
+    ]
+
+    @IBAction func nextQuestionDidTap(_ sender: AnyObject) {
+        
+        let numberOfQuestions = quesArr.count
+        if currentQuestionIndex < (numberOfQuestions - 1) {
+            currentQuestionIndex += 1
+        } else {
+            currentQuestionIndex = 0
+        }
+        CountLbl.text = String(currentQuestionIndex)
+        questionLabel.text = quesArr[currentQuestionIndex]
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    
 }
 
